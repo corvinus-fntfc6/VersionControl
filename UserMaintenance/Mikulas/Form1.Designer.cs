666af5efd1dbd66875b0fec566748b5aa7e96d37
Boglárka Száday <boglarka.szaday@stud.uni-corvinus.hu>
2021-11-12 21:41:10 +0100

    szinvalaszto_gomb__

--- UserMaintenance/Mikulas/Form1.Designer.cs
+++ UserMaintenance/Mikulas/Form1.Designer.cs
@@ -36,6 +36,7 @@ namespace Mikulas
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -80,17 +81,28 @@ namespace Mikulas
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(219, 13);
+            this.label1.Location = new System.Drawing.Point(368, 13);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(89, 17);
             this.label1.TabIndex = 3;
             this.label1.Text = "Coming next:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(219, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 58);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -110,6 +122,7 @@ namespace Mikulas
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
