c74b79527f7580c58095f59e665286f3d1b0fea2
Boglárka Száday <boglarka.szaday@stud.uni-corvinus.hu>
2021-11-12 20:37:55 +0100

    absztrakcio_felhasznalasa_a_gyarakban_kesz

--- UserMaintenance/Mikulas/Entities/CarFactory.cs
+++ UserMaintenance/Mikulas/Entities/CarFactory.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Mikulas.Entities
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
