bc690b44091e4f4908f061b3d8930c42359637ac
Boglárka Száday <boglarka.szaday@stud.uni-corvinus.hu>
2021-12-03 23:05:43 +0100

    classok

---
+++ week09_sokexcel/Entities/BirthProbability.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class BirthProbability
+{
+	public byte Age { get; set; }
+	public int NbrOfChildren { get; set; } 
+	public double P { get; set; }
+
+	public BirthProbability()
+	{
+
+	}
+}
---
+++ week09_sokexcel/Entities/DeathProbability.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Deathprobability
+{
+	public Gender Gender { get; set; }
+	public byte Age { get; set; }
+	public double P { get; set; }
+
+	public Deathprobability()
+	{
+	}
+}
